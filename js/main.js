bee06c879fd52004f86750f3f235ddfa13672ca0
sharendus <sharendo.lyuba@mail.ru>
2021-09-29 22:02:58 +0300

    Функции, возвращающие случайные числа

---
+++ js/main.js
@@ -0,0 +1,28 @@
+//Получение случайного целого числа в заданном диапазоне
+function getRandomFromRange (min ,max) {
+  if (min < max && min >=0) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  } else {
+    console.log('Ошибка');
+    if (min === max) {console.log('Значение min не должно быть равно max')}
+    if (min > max) {console.log('Значение min должно быть меньше max')}
+    if (min < 0) {console.log('Значение min не должно быть меньше 0')}}
+}
+
+getRandomFromRange(min ,max);
+
+
+//Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно
+function getRandomFromRangeToFixed(min, max) {
+  if (min < max && min >=0) {
+    return Math.random() * (max - min + 1) + min;
+  }
+}
+getRandomFromRangeToFixed (min, max)
+
+
+
+
+
