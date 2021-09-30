602e67bff190225ac18fd326bc8bedd3009e90a4
sharendus <sharendo.lyuba@mail.ru>
2021-09-30 21:21:16 +0300

    проверка линтера

--- js/main.js
+++ js/main.js
@@ -4,15 +4,8 @@ function getRandomFromRange (min ,max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
-  } else {
-    console.log('Ошибка');
-    if (min === max) {console.log('Значение min не должно быть равно max')}
-    if (min > max) {console.log('Значение min должно быть меньше max')}
-    if (min < 0) {console.log('Значение min не должно быть меньше 0')}}
+  }
 }
-
-getRandomFromRange(min ,max);
-
 
 //Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно
 function getRandomFromRangeToFixed(min, max) {
@@ -20,15 +13,8 @@ function getRandomFromRangeToFixed(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor ((Math.random() * (max - min + 1) + min) * 100) / 100;
-  } else {
-    console.log('Ошибка');}
+  }
 }
-getRandomFromRangeToFixed (min, max)
 
-
-
-//    min = alert((min).toFixed(n));
-//max = alert((max).toFixed(n));
-
-
-
+getRandomFromRange(1,3);
+getRandomFromRangeToFixed(1,3);
