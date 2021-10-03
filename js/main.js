497bb56105a2a713d63395e163e84c66eb93d4b1
sharendus <sharendo.lyuba@mail.ru>
2021-10-03 09:04:23 +0300

    исправила функции

--- js/main.js
+++ js/main.js
@@ -4,17 +4,26 @@ function getRandomFromRange (min ,max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
+  } else {
+    if (min === max) {return('Значение min не должно быть равно max');}
+    if (min > max) {return('Значение min должно быть меньше max');}
+    if (min < 0) {return('Значение min не должно быть мегбше 0');}
   }
 }
 
 //Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно
-function getRandomFromRangeToFixed(min, max) {
+function getRandomFromRangeToFixed(min, max, numberOfSimbols ) {
   if (min < max && min >=0) {
     min = Math.ceil(min);
     max = Math.floor(max);
-    return Math.floor ((Math.random() * (max - min + 1) + min) * 100) / 100;
+    const nOS = Math.pow(10, numberOfSimbols);
+    return Math.floor ((Math.random() * (max - min + 1) + min) * nOS) / nOS;
+  } else {
+    if (min === max) {return('Значение min не должно быть равно max');}
+    if (min > max) {return('Значение min должно быть меньше max');}
+    if (min < 0) {return('Значение min не должно быть меньше 0');}
   }
 }
 
 getRandomFromRange(1,3);
-getRandomFromRangeToFixed(1,3);
+getRandomFromRangeToFixed(1,3,2);
