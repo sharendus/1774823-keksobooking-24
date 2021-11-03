5b89c66c97e837d4b7ed33d3a4f8bc4ef7299bd5
sharendus <sharendo.lyuba@mail.ru>
2021-11-03 11:00:11 +0300

    add module page

---
+++ js/page-status.js
@@ -0,0 +1,37 @@
+const form = document.querySelector('.ad-form');
+const formFieldset = form.querySelectorAll('.ad-form__element');
+const map = document.querySelector('.map__filters');
+const mapSelect = map.querySelectorAll('select');
+const mapFieldset = map.querySelector('fieldset');
+
+
+const creatInactive = function () {
+  form.classList.add('.ad-form--disabled');
+  for (let i = 0; i < formFieldset.length; i++) {
+    formFieldset[i].disabled = true;
+  }
+
+  map.classList.add('.ad-form--disabled');
+  for (let i = 0; i < mapSelect.length; i++) {
+    mapSelect[i].disabled = true;
+  }
+
+  mapFieldset.disabled = true;
+};
+
+
+const creatActive = function () {
+  form.classList.remove('.ad-form--disabled');
+  for (let i = 0; i < formFieldset.length; i++) {
+    formFieldset[i].disabled = false;
+  }
+
+  map.classList.remove('.ad-form--disabled');
+  for (let i = 0; i < mapSelect.length; i++) {
+    mapSelect[i].disabled = false;
+  }
+
+  mapFieldset.disabled = false;
+};
+
+export {creatInactive, creatActive};
