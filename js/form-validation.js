c3d785d569782fd0cd50d44535deb254c7cfddb3
sharendus <sharendo.lyuba@mail.ru>
2021-11-06 11:55:10 +0300

    timeout timein

--- js/form-validation.js
+++ js/form-validation.js
@@ -1,19 +1,23 @@
+const MIN_TITLE_LENGTH = 30;
+const MAX_TITLE_LENGTH = 100;
+const MAX_PRICE_LENGTH = 1000000;
+
+
 const titleInputForm = document.querySelector('#title');
 const priceInputForm = document.querySelector('#price');
 const typeSelectForm = document.querySelector('#type');
 const roomSelectForm = document.querySelector('#room_number');
 const capacitySelectForm = document.querySelector('#capacity');
+const timeinSelectForm = document.querySelector('#timein');
+const timeoutSelectForm = document.querySelector('#timeout');
 
-const MIN_TITLE_LENGTH = 30;
-const MAX_TITLE_LENGTH = 100;
-const MAX_PRICE_LENGTH = 1000000;
 
 titleInputForm.addEventListener('input', () => {
   const valueLength = titleInputForm.value.length;
   if (valueLength < MIN_TITLE_LENGTH) {
-    titleInputForm.setCustomValidity('Ещё ' + (MIN_TITLE_LENGTH - valueLength) + ' симв.');
+    titleInputForm.setCustomValidity(`Ещё ${  MIN_TITLE_LENGTH - valueLength  } симв.`);
   } else if (valueLength > MAX_TITLE_LENGTH) {
-    titleInputForm.setCustomValidity('Удалите лишние ' + (valueLength - MAX_TITLE_LENGTH) + ' симв.');
+    titleInputForm.setCustomValidity(`Удалите лишние ${  valueLength - MAX_TITLE_LENGTH  } симв.`);
   } else {
     titleInputForm.setCustomValidity('');
   }
@@ -46,9 +50,9 @@ typeSelectForm.addEventListener('change', () => {
 
 priceInputForm.addEventListener('input', () => {
   if (priceInputForm.validity.rangeOverflow) {
-    priceInputForm.setCustomValidity('Максимальная цена ' + MAX_PRICE_LENGTH);
+    priceInputForm.setCustomValidity(`Максимальная цена ${  MAX_PRICE_LENGTH}`);
   } else if (priceInputForm.validity.rangeUnderflow) {
-    priceInputForm.setCustomValidity('Минимальная цена ' + priceInputForm.min);
+    priceInputForm.setCustomValidity(`Минимальная цена ${  priceInputForm.min}`);
   } else if (priceInputForm.validity.valueMissing) {
     priceInputForm.setCustomValidity('Обязательное поле');
   } else {
@@ -75,6 +79,15 @@ roomSelectForm.addEventListener('change', () => {
       } else {
         capacitySelectForm[i].disabled = true;
       }
+    }
+  }
+});
+
+timeinSelectForm.addEventListener('change', () => {
+  for (let i = 0; i < timeinSelectForm.length; i++) {
+    if (timeinSelectForm[i].selected) {
+      timeoutSelectForm[i].selected = true;
+      //timeoutSelectForm.disabled = true;
     }
   }
 });
